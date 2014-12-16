f18a9a04a1270b63e9d72ddc78f2008f5eb87c4e
diedona <diedona@gmail.com>
2014-12-15 23:57:46 -0200

    permitindo adicionar itens

--- js/app/controller/BodyCtrl.js
+++ js/app/controller/BodyCtrl.js
@@ -6,6 +6,16 @@
         ProductService.GetAll().then(function(d) {
             $scope.Products = d;
         });
+            
+        $scope.SaveProduct = function() {
+            
+            if($scope.frmAddProduct.$valid)
+            {
+                $scope.Products.push(angular.copy($scope.Product));
+                $scope.Product = {};
+            }
+            
+        };
         
     }]);
     
